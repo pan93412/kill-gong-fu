9a08e773407c90709056265d2a5b84d8bca786a5
pan93412 <pan93412@gmail.com>
2022-03-23 22:40:41 +0800

    refactor: better logic

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use teloxide::prelude2::*;
 // use pinyin::{ToPinyin, ToPinyinMulti};
 
 /// 判斷是「共富國際」的可能率。
-/// 
+///
 /// 回傳值將會是一個介於 0~1 的數字。
 fn get_gong_fu_possibility(name: &str) -> f32 {
+    let name = name.replacen('-', "", 1);
     let matched_characters = name
+        .as_str()
         .to_pinyin()
         .flatten()
         .map(|v| v.plain())
@@ -16,9 +18,15 @@ fn get_gong_fu_possibility(name: &str) -> f32 {
 
     {
         // https://www.desmos.com/calculator/wmvf92efik
+
         let x = matched_characters as f32;
-        
-        (4.0 - (x - 4.0).abs()) / 4.0
+        let n = name.chars().count() as f32;
+
+        if n > 0.0 {
+            1.0 - (1.0 - (x / n)).abs()
+        } else {
+            0.0
+        }
     }
 }
 
@@ -31,7 +39,25 @@ async fn main() {
 
     teloxide::repls2::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
         let possibility = message.text().map(get_gong_fu_possibility).unwrap_or(0.0);
-        bot.send_message(message.chat.id, possibility.to_string()).await?;
+
+        if possibility == 1.0 {
+            log::info!("spam possibility = 1.0, kick out!");
+
+            if let Some(sender_chat) = message.sender_chat() {
+                bot.kick_chat_member(message.chat.id, sender_chat.id)
+                    .await?;
+                bot.send_message(
+                    message.chat.id,
+                    format!(
+                        "{} 極有可能是廣告 - 已自動踢出，可自動拉回。",
+                        sender_chat.title().unwrap_or("<?>")
+                    ),
+                )
+                .await?;
+            }
+        }
+        bot.send_message(message.chat.id, possibility.to_string())
+            .await?;
         respond(())
     })
     .await;
@@ -57,54 +83,12 @@ mod tests {
 
     #[test]
     fn test_get_gong_fu_possibility_100_percent() {
-        assert_eq!(
-            super::get_gong_fu_possibility("共富國際"),
-            1.0
-        );
-    }
-
-    #[test]
-    fn test_get_gong_fu_possibility_75_percent() {
-        assert_eq!(
-            super::get_gong_fu_possibility("共富國"),
-            0.75
-        );
-    }
-
-    #[test]
-    fn test_get_gong_fu_possibility_50_percent() {
-        assert_eq!(
-            super::get_gong_fu_possibility("共富"),
-            0.5
-        );
-
-        assert_eq!(
-            super::get_gong_fu_possibility("不是共富"),
-            0.5
-        );
-    }
-
-    #[test]
-    fn test_get_gong_fu_possibility_25_percent() {
-        assert_eq!(
-            super::get_gong_fu_possibility("共"),
-            0.25
-        );
-        assert_eq!(
-            super::get_gong_fu_possibility("國家"),
-            0.25
-        );
+        assert_eq!(super::get_gong_fu_possibility("共富-國際"), 1.0);
     }
 
     #[test]
     fn test_get_gong_fu_possibility_0_percent() {
-        assert_eq!(
-            super::get_gong_fu_possibility("Hello, World!"),
-            0.0
-        );
-        assert_eq!(
-            super::get_gong_fu_possibility("嗨"),
-            0.0
-        );
+        assert_eq!(super::get_gong_fu_possibility("Hello, World!"), 0.0);
+        assert_eq!(super::get_gong_fu_possibility("嗨"), 0.0);
     }
 }
